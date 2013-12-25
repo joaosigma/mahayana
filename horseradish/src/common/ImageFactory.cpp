cdcb9bd8a908cecd16fe2e5d8fcab9def57cc870
João Ramos <joao.sigma@gmail.com>
2013-12-25 23:33:49 +0000

    Updated jpeglib to version 9 and refactored the code

--- horseradish/src/common/ImageFactory.cpp
+++ horseradish/src/common/ImageFactory.cpp
@@ -2,7 +2,7 @@
 #include "imageFactory\ifTGA.hpp"
 #include "imageFactory\ifHDRI.hpp"
 
-#include "libs\libJPEG\jpeglib.h"
+#include "libs\jpeglib\jpeglib.h"
 #include "libs\lodepng\lodepng.h"
 
 ///§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
