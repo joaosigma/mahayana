0452af8606057866c04a47b7075a539dec90cd9f
João Ramos <joao.sigma@gmail.com>
2015-06-14 18:20:06 +0100

    Updated stb and lodepng libs and fixed squirrel build.

--- horseradish/src/libs/squirrel/sqstdstream.cpp
+++ horseradish/src/libs/squirrel/sqstdstream.cpp
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <squirrel.h>
-#include <sqstdio.h>
-#include <sqstdblob.h>
+#include "squirrel.h"
+#include "sqstdio.h"
+#include "sqstdblob.h"
 #include "sqstdstream.h"
 #include "sqstdblobimpl.h"
 
